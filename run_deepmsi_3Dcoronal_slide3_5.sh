228a8942ef21d5dd3b9bdf989305ad0f81644147
richardxie1119 <xieyuxuan1996@gmail.com>
2023-12-19 11:05:15 -0600

    Add files via upload

---
+++ run_deepmsi_3Dcoronal_slide3_5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export PATH=%PATH:/mnt/c/Users/Richard/Anaconda3/envs/deepmsi
+
+out_dir="./processed_data"
+path_files="./file_dir_coronal_3D_slide3_5.json"
+decoder_dir="./saved_model/coronal3D_latent32_epoch10_decoder"
+regressor_dir="./saved_model/coronal3D_latent32_epoch10_regressor"
+recon_ROI=(
+    "R00"
+    "R01"
+    "R02"
+    "R03"
+    "R04"
+    "R05"
+    "R06"
+    "R07"
+    "R08"
+    "R09"
+    "R10"
+)
+mz_range="150 1100"
+if_process_raw="True"
+if_simu='False'
+embedding='True'
+
+for ((i=0;i<${#recon_ROI[@]};++i)); do
+    printf "Processing data for region %s ...\n" "${recon_ROI[i]}"
+    python.exe deep_recon.py  --path_file $path_files --out_dir $out_dir --embedding $embedding --recon_ROI ${recon_ROI[i]}
+     --decoder_dir $decoder_dir --regressor_dir $regressor_dir --mz_range $mz_range  --if_process_raw $if_process_raw --if_simu $if_simu
+done
+
